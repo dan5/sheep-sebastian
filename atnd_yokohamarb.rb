06458442666441b92deb7d43ac936206b7ac58e3
dan5 <dan5.ya@gmail.com>
2011-06-25 16:13:25 +0900

    todo追加

---
+++ atnd_yokohamarb.rb
@@ -0,0 +1,18 @@
+def atnd_yokohamarb
+  {
+    :title => "Yokohama.rb 第?回",
+    :catch => "",
+    :description => "",
+    :started_at => "2011-07-02",
+    :started_at_time => "17:00",
+    :ended_at => "2011-07-02",
+    :ended_at_time => "21:00",
+    :limit => "25",
+    :place => "横浜市神奈川地区センター情報（和室）",
+    :address => "神奈川区神奈川本町8-1",
+    :url => "http://sites.google.com/site/yokohamarb/",
+    :image => "",
+    :recommended_twitter_account => "",
+    :hash_tag => "#yokohamarb",
+  }
+end
