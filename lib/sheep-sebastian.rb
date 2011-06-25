1addf06aee9b0e61696669bdd020c1a67697737b
dan5 <dan5.ya@gmail.com>
2011-06-25 21:15:28 +0900

    db実装と設定ファイル作成

--- lib/sheep-sebastian.rb
+++ lib/sheep-sebastian.rb
@@ -1,7 +1,58 @@
 require "sheep-sebastian/version"
+require 'twitter'
+require 'yaml'
 
-module Sheep
-  module Sebastian
-    # Your code goes here...
+class SheepSebastian
+  module DB
+    def self.load(filename, default)
+      db = _load(filename, default)
+      def db.filename=(fname) @fname = fname end
+      def db.save() File.open(@fname, 'w') {|f| f.write(self.to_yaml) } end
+      db.filename = filename
+      db
+    end
+
+    def self._load(filename, default)
+      YAML::load(File.read(filename)) rescue default
+    end
+  end
+
+  def tweets(db)
+    since_id = db[:since_id]
+    opts = since_id ? { :since_id => since_id } : {}
+    tweets = Twitter.user_timeline('dan5ya', opts)
+    unless tweets.empty?
+      db[:since_id] = tweets.first.id
+      db.save
+    end
+    tweets
+  end
+
+  def hit?(text, recipe)
+    recipe[:keywords].each {|keyword| text[keyword] or return false }
+    return true
+  end
+
+  def atend_new_post(text, recipe)
+    tmp, month, day = text.match(/(1?\d)\/([1-3]?\d)/).to_a
+    tmp, hour, minutes = text.match(/([12]?\d):([0-5]?\d)/).to_a
+    at = sprintf("%04d-%02d-%02d", Time.now.year, month, day) # todo: next year...
+    at_time = sprintf("%02d:%02d", hour, minutes)
+    template = Post_template
+    template[:started_at] = at
+    template[:started_at_time] = at_time
+    template[:ended_at] = at
+    p text
+    p template
+  end
+
+  def initialize(db_filename = nil)
+    db = DB.load(db_filename || ARGV.first, {:since_id => nil})
+    recipe = Recipe
+    tweets(db).each do |tweet|
+      if hit?(tweet.text, recipe)
+        __send__ recipe[:handle], tweet.text, recipe
+      end
+    end
   end
 end
--- lib/sheep-sebastian/version.rb
+++ lib/sheep-sebastian/version.rb
@@ -1,5 +1,5 @@
 module Sheep
   module Sebastian
-    VERSION = "0.0.1"
+    VERSION = "0.0.2"
   end
 end
