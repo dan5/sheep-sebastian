5db642ba950f4bd934f1c4db111b3e21bc319d97
dan5 <dan5.ya@gmail.com>
2011-06-25 09:42:46 +0900

    add README

---
+++ lib/sheep-sebastian.rb
@@ -0,0 +1,7 @@
+require "sheep-sebastian/version"
+
+module Sheep
+  module Sebastian
+    # Your code goes here...
+  end
+end
---
+++ lib/sheep-sebastian/version.rb
@@ -0,0 +1,5 @@
+module Sheep
+  module Sebastian
+    VERSION = "0.0.1"
+  end
+end
